2bf77c2ca13176997533dd61d242cb156b796a99
Amandeep Gill <amandeepgill@Amandeeps-MacBook-Pro.local>
2023-12-26 23:19:25 -0500

    updated the code to use styled component instead of scss

--- crwn-clothing/src/routes/shop/shop.component.jsx
+++ crwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,18 +1,14 @@
-import { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import "./shop.styles.scss";
-
-import { ProductsContext } from "../../contexts/products.context";
-import ProductCart from "../../components/product-card/product-card.component";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <ProductCart key={product.id} product={product} />
-      ))}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
